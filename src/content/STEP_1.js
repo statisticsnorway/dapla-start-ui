61076c44226bf094b8861498289da9fb916b38d1
Olve Drageset <olve.drageset@gmail.no>
2022-02-01 15:48:32 +0100

    more name creation guidelines

--- src/content/STEP_1.js
+++ src/content/STEP_1.js
@@ -1,7 +1,7 @@
 export const STEP_1 = {
   TEXT: <>
     <p>
-      Navnet på et Dapla-team bør reflektere <em>domene - subdomene</em>.
+      Navnet på et Dapla-team bør reflektere <em>domene og subdomene</em>.
       Her er noen eksempler på eksisterende teamnavn:
     </p>
     <ul>
@@ -9,5 +9,18 @@ export const STEP_1 = {
       <li>Team Skatt Næring</li>
       <li>Team Kostra KVM</li>
     </ul>
+    <p>"Skatt" vil da være et eksempel på et domene, mens "Person" og "Næring" er eksempler på subdomener under samme domene.</p>
+    <h2>Lengde og forkortelser</h2>
+    <p>
+      Delen av navnet som beskriver domene og subdomene (for eksempel "Skatt Person"), kan i seg selv IKKE overskride 25 tegn!
+      Dersom navnet til teamet er langt anbefaler vi å forkorte den mest generelle delen av navnet til et trebokstavers "flyplassnavn".
+      For eksempel, "Grunnopplæring Grunnskole" som er 25 tegn kan bli til "GRO Grunnskole" som er 14 tegn.
+      Dersom domenet allerede er en forkortelse eller sammenslåing (f.eks. "Kostra" for "Kommune Stat Raportering") kan
+      man bruke en "flyplassnavn" for subdomenet istedenfor for å korte ned navnet, slik "Kostra KVM" har gjort.
+    </p>
+    <p>
+      <b>NB:</b> Dersom det finnes andre team i deres domene som er eller skal på Dapla,
+      vær sikker på at dere bruker samme "flyplassnavn" for domenet når dere opretter team.
+    </p>
   </>
 }
