b583a866114729ac1a2659afdde14e85b77b357e
mmj-ssb <mmj@ssb.no>
2022-02-16 11:51:51 +0100

    Rename access levels

--- src/content/WIZARD.js
+++ src/content/WIZARD.js
@@ -30,7 +30,7 @@ export const WIZARD = {
   },
   CONSUMERS: {
     title: 'Konsumenter',
-    description: 'Kun Lesetilgang til utdata.',
+    description: 'Kun lesetilgang til delte data.',
     ref: 'consumers'
   },
   OTHER_INFO: {
@@ -51,7 +51,7 @@ export const WIZARD = {
       {
         label: 'Transfer Service',
         value: 'transfer_service',
-        description: 'Overføre filer mellom prodsonen og Dapla'
+        description: 'Overføre filer mellom Produksjonssonen og Dapla'
       }
     ],
     ref: 'enabled_services'
